9aaba1484842764e3e5bf69c71cf459bc415e6c4
Kamil Rocki <kmrocki@MacBook-Pro-4.local>
2020-01-06 14:02:20 -0800

    rewrite

--- as6502.py
+++ as6502.py
@@ -3,126 +3,94 @@
 
 import argparse
 import re
-from defs import opcodes
+from defs import *
 
-def assemble(lines, opt, pc=0x600):
+def assemble(lines, opt):
 
-  if opt.verbose:
-    print('Assembling...')
+  pattern = '^\s*(?P<expr>(?P<label>[a-z]*):|(?P<op>[a-zA-z]{3})(?P<args>.*?))?(?P<comment>;.*)?$'
 
-  pattern = '^\s*((?P<label>[A-za-z0-9_]+):)|(?P<expr>(?P<op>[A-Za-z]{3}?)\s*(?P<argraw>(?P<imm>#?)(?P<hex>\$?)(?P<argval>[0-9a-fA-F]{1,4})|(?P<arglab>[A-za-z0-9_]*?)))?\s*(\;.*)?$'
-
-  data = {}
   instr = {}
-
-  pos = 0
-
   labels = {}
+  data = {}
+  pc = opt.pc
 
   for i,l in enumerate(lines):
     m = re.search(pattern, l)
     if opt.verbose:
-      print('line {}=[{}]'.format(i,l))
-      print('regex groups={}'.format(m.groups()))
+      print('\nline {}=[{}]'.format(i,l))
+
+    if opt.verbose:
+      print('labels={}'.format(labels))
 
     # syntax: expression = op (space arg)?
     expr=m.group('expr')        # entire expression
     op=m.group('op')            # op
     label=m.group('label')      # label
+    args=m.group('args')        # args
+    comment=m.group('comment')  # comment
 
     if opt.verbose:
-      print('label={}, expr={}, op={}'.format(label, expr, op))
+      print(' label=[{}], expr=[{}], op=[{}], args=[{}], comm=[{}]'.format(
+        label, expr, op, args, comment))
 
     if label:
       if opt.verbose:
-        print('inserting label [{}]'.format(label))
-      labels[label] = pos
+        print('  * lab [{}] pc={:x}'.format(label, pc))
+      labels[label] = ' $'+str(hex(pc))
+
+    # is arg a label ?
+    if args:
+      args_str = args.strip()
+      if args_str in labels:
+        args = labels[args_str]
+        if opt.verbose:
+          print('  * args found as a label [{}] -> [{}]'.format(args_str, args))
 
     if op==None: # empty or comments
       continue
 
-    is_bra=op in ['BPL','BMI','BVC',
-                  'BCC','BCS','BNE',
-                  'BEQ']
-
-    if opt.verbose:
-      print('is_bra={}'.format(is_bra))
-
-    argraw=m.group('argraw')
-    if opt.verbose:
-      print('argraw={}'.format(argraw))
-
-    is_unary=True if argraw==None else len(argraw)==0 # no args
-
-    if opt.verbose:
-      print('is_unary={}'.format(is_unary))
-
-    if is_unary: # no arguments
-      encoding=opcodes[op][10] # sngl
-      argbytes=0
-
-    else: # 1 or more arguments
-      is_imm=m.group('imm')=="#"
-      is_hex=m.group('hex')=="$"
-      arglab_str = m.group('arglab')
-      argval_str = m.group('argval')
-      if opt.verbose:
-        print('is_imm={}, is_hex={}, '
-        'arglab_str={}, argval_str={}'.format(
-        is_imm, is_hex, arglab_str, argval_str))
-
-      argval=int(argval_str, 16) if is_hex \
-        else int(argval_str, 10) if is_imm \
-        else labels[arglab_str]
-
-      arglen=len(argval_str) if argval_str else 1
-      if is_bra:
-        encoding=opcodes[op][11]
-        argbytes=1
-        argval=(argval-pos-2)
-      elif is_imm:
-        encoding=opcodes[op][0] #imm
-        argbytes=1
-      else:
-        if arglen<=2:
-          encoding=opcodes[op][1] #zp
-          argbytes=1
-        else:
-          encoding=opcodes[op][4] #abs
-          argbytes=2
-
-    if opt.strip:
-      print('{}'.format(expr))
-
-    if opt.verbose:
-      xs=opcodes[op]
-      x_str = [hex(x) if x!=None else '' for x in xs]
-      print('{:} {}'.format(op, x_str))
-
-    if encoding==None:
-      print('uh-oh')
     else:
-      data[pos] = encoding
-      addr=pos
-      s='0x{:04x}: {:02x}'.format(pos, encoding)
-      pos+=1
+      encodings=list(filter(lambda y: y[1]!=None, [(i,e) for i,e in enumerate(opcodes[op.lower()])]))
+
+      mode=None
+      for m, e in encodings:
+        p=re.search(modes[m][2], args)
+        if p!=None:
+          mode = modes[m]
+          if mode[0] != 10:
+            is_hex = p.group(1)
+            rawval = p.group(2)
+
+      if None==mode:
+        print('uh-oh: could not find addressing mode')
+        return None
+
+      val=int(rawval, 16) if is_hex \
+      else int(rawval, 10)
+
+      argbytes=mode[3]
+      encoding=opcodes[op][mode[0]]
+
+      if opt.verbose:
+        print('  * mode [{}], opcode [0x{:02x}], argbytes [{}] '.format(mode[1], encoding, argbytes))
+        print('  * rawval [{}], ishex [{}], val [0x{:04x}] '.format(rawval, is_hex, val))
+
+      if mode[1]=='rel':
+         val=(val-pc-2)
+      addr=pc
+      s=[encoding]
+      pc+=1
       for b in range(argbytes):
-        data[pos] = (argval >> (8*b)) & 0xff
-        s+=' {:02x}'.format(data[pos])
-        pos+=1
+        data[pc] = (val >> (8*b)) & 0xff
+        s.append(data[pc])
+        pc+=1
 
-      instr[addr] = s
-      if opt.verbose: print(s + '\n')
+      instr[addr] = {}
+      instr[addr]['src'] = '{:20} {:3} {:10}'.format(expr, op, args)
+      instr[addr]['obj'] = s
+      if opt.verbose: print('  * assembled [{}]'.format(s))
 
-  prog_len=max(data)+1
-  mem = [0x00] * prog_len
-  for k in data:
-    mem[k] = data[k]
-
-  if opt.verbose:
-    print('Assembled, {} Bytes'.format(prog_len))
-
-  return mem, instr
+  return instr, pc - opt.pc, labels
 
 if __name__ == "__main__":
 
@@ -136,6 +104,8 @@ if __name__ == "__main__":
      '--verbose', action="store_true", help='verbose')
   parser.add_argument('-strip',
      '--strip', action="store_true", help='remove whitespace and comments')
+  parser.add_argument('-p',
+     '--pc', type=hex, default=0x600, help='default entry point')
 
   opt = parser.parse_args()
   if opt.verbose: print(opt)
@@ -149,17 +119,22 @@ if __name__ == "__main__":
   lines = [line.rstrip('\n') for line in lines]
 
   if opt.verbose:
-    print('{:} read, {:} lines'.format(ifile, len(lines)))
-    [print('line {:}: [{:}]'.format(i, l))
-       for i,l in enumerate(lines)]
+    print('{:} read, {:} lines, assembling...'.format(ifile, len(lines)))
 
-  mem, instr = assemble(lines, opt, pc=0x600)
+  instr, n_bytes, labels = assemble(lines, opt)
 
-  if ofile:
-    with open(ofile, 'wb') as fp:
-      fp.write(bytearray(mem))
+  if None==instr:
+    print('Assembly failed!')
   else:
-    for k,i in enumerate(instr):
-      print('[{:02d}] {:}'.format(k, instr[i]))
-    print('')
-    print(''.join('{:02x}'.format(m) for m in mem))
+    if ofile:
+      mem = [0] * n_bytes
+      for k,i in enumerate(instr):
+        for n in range(len(instr[i]['obj'])):
+          mem[i - opt.pc + n] = instr[i]['obj'][n]
+      with open(ofile, 'wb') as fp:
+        fp.write(bytearray(mem))
+    else:
+      print('Assembled, {} Bytes'.format(n_bytes))
+      print('labels', labels)
+      for k,i in enumerate(instr):
+        print('[{}] -> [0x{:04x}] {:}'.format(instr[i]['src'], i, ['0x{:02x}'.format(x) for x in instr[i]['obj']]))
