018facf545eb18917e9296c74099ed829e3aeb0d
Kamil Rocki <kamil.rocki@neuralink.com>
2021-10-21 12:52:21 -0700

    regex

--- cc/main.cc
+++ cc/main.cc
@@ -5,6 +5,7 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <regex>
 #include <map>
 
 void split(const std::string &s, const std::string delim, std::vector<std::string> &tokens) {
@@ -34,17 +35,80 @@ void print_vector(std::vector<T> &v) {
 struct codeline {
   size_t line_no;
   std::string label;
+  std::string op;
+  std::string arg;
   std::string code;
   std::string comment;
 };
 
 void print_codeline(struct codeline &l) {
   std::cout << std::right << std::setw(4) << l.line_no << " | " << std::left << std::setw(10) << l.label << std::left <<
-    std::setw(25) << l.code << std::left << std::setw(25) << l.comment << "\n";
+    std::setw(10) << l.op << std::left << std::setw(10) << l.arg << std::left << std::setw(25) << l.code << std::left << std::setw(25) << l.comment << "\n";
 };
 
 bool string_is_empty(std::string &str) {
   return (str.find_first_not_of(' ') == std::string::npos);
+}
+
+std::regex
+line_expr("^\\s*((([A-Za-z0-9]+):)?\\s*(([a-zA-z]{3})(\\s+(.*?))?)?)?(;.*)?$");
+//line_expr("^\\s*(((?P<label>[A-Za-z0-9]+):)?\\s*(?P<expr>(?P<op>[a-zA-z]{3})(\\s+(?P<args>.*?))?)?)?(?P<comment>;.*)?$");
+
+struct codeline parse_regex(std::string &line) {
+
+  std::match_results<std::string::const_iterator > mr;
+  std::regex_search( line, mr, line_expr );
+  std::cout << "line = " << line << " | " << mr.size() << " matches\n";
+
+  for( std::size_t index = 0; index < mr.size(); ++index ){
+    std::cout << index << " -> " << mr[ index ] << "\n";
+  }
+
+  //mr[0] - entire match (lhs + comment)
+  //mr[1] - match lhs
+  //mr[2] - label + ":"
+  //mr[3] - label
+  //mr[4] - whole instruction
+  //mr[5] - instruction without operand
+  //mr[6] - operand untrimmed
+  //mr[7] - operand trimmed
+  //mr[8] - comment (rhs)
+
+  struct codeline cl{0, mr[3], mr[5], mr[7], mr[4], mr[8]};
+  return cl;
+}
+
+struct codeline parse_split(std::string &line) {
+
+  std::string text, comment, label, code;
+
+  /* strip comments */
+  auto end = line.find(";");
+  if (end != std::string::npos) {
+    text = line.substr(0, end);
+    comment = line.substr(end);
+  } else {
+    text = line;
+  }
+
+  /* check if label */
+  end = text.find(":");
+  if (end != std::string::npos) {
+    label = text.substr(0, end+1);
+    code = text.substr(end+1);
+  } else {
+    code = text;
+  }
+
+  /* trim leading whitespace */
+  end = code.find_first_not_of(' ');
+  if (end == std::string::npos) {
+    code = "";
+  } else {
+    code = code.substr(end);
+  }
+
+  return {0, label, "", "", code, comment};
 }
 
 int main(int argc, char **argv) {
@@ -61,6 +125,7 @@ int main(int argc, char **argv) {
   std::string line;
 
   std::map<size_t, struct codeline> lines;
+  std::map<std::string, size_t> labels;
 
   // start with line no 1, not 0
   size_t line_no = 1;
@@ -68,50 +133,37 @@ int main(int argc, char **argv) {
   while (std::getline(infile, line)) {
     std::istringstream iss(line);
 
-    std::string label = "";
-    std::string comment = "";
-    std::string text = "";
-    std::string code = "";
+    //auto [_, label, code, comment] = parse_split(line);
+    auto [_, label, op, arg, code, comment] = parse_regex(line);
 
-    /* strip comments */
-    auto end = line.find(";");
-    if (end != std::string::npos) {
-      text = line.substr(0, end);
-      comment = line.substr(end);
-    } else {
-      text = line;
-    }
-
-    /* check if label */
-    end = text.find(":");
-    if (end != std::string::npos) {
-      label = text.substr(0, end+1);
-      code = text.substr(end+1);
-    } else {
-      code = text;
-    }
-
-    /* trim leading whitespace */
-    end = code.find_first_not_of(' ');
-    if (end == std::string::npos) {
-      code = "";
-    } else {
-      code = code.substr(end);
+    if (label.length() > 0) {
+      if (labels.find(label) != labels.end()) {
+        printf("ERROR! label %s from line %zu redefined (line %zu)\n",
+          label.c_str(), labels[label], line_no);
+      }
+      labels[label] = line_no;
     }
 
     if (!string_is_empty(code) or comment.length() > 0 or label.length() > 0) {
-      dbg << std::right << std::setw(4) << line_no << " | " << std::left << std::setw(10) << label << std::left << std::setw(25) << code << std::left << std::setw(25) << comment << "\n";
-      lines[line_no] = {line_no, label, code, comment};
+      dbg << std::right << std::setw(4) << line_no << " | " << std::left << std::setw(10) << label << std::left <<
+        std::setw(10) << op << std::left << std::setw(10) << arg << std::left << std::setw(25) << code << std::left << std::setw(25) << comment << "\n";
+      lines[line_no] = {line_no, label, op, arg, code, comment};
     }
 
     line_no++;
   }
 
-  std::cout << std::setw(4) << "LINE" << " | " << std::left << std::setw(10) << "LABEL" << std::left << std::setw(25) <<
-    "CODE" << std::left << std::setw(25) << "COMMENT" << "\n";
+  std::cout << std::setw(4) << "LINE" << " | " << std::left << std::setw(10) << "LABEL" << std::left << std::setw(10) <<
+    "OP" << std::left << std::setw(10) << "ARG" << std::left << std::setw(25) << "CODE" << std::left << std::setw(25) << "COMMENT" << "\n";
+
   for (auto &c: lines) {
     print_codeline(c.second);
   };
+
+  std::cout << "\nlabels: \n";
+  for (auto &c: labels) {
+    std::cout << c.first << "@" << c.second << "\n";
+  }
 
   infile.close();
   dbg.close();
